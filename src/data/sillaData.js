7c1003b7819e20ce5565bae209075e141c64702d
camilofinal <camilofinal@gmail.com>
2025-06-20 17:38:02 +0000

    feat: Implement chair product cards on Sillas page

---
+++ src/data/sillaData.js
@@ -0,0 +1,20 @@
+const sillas = [
+    {
+      name: 'Silla Rústica de Madera',
+      image: '/src/assets/images/sillas/silla_madera_rustica.jpg'
+    },
+    {
+      name: 'Silla Vintage Elegante',
+      image: '/src/assets/images/sillas/—Pngtree—old wood chair_14665502.png'
+    },
+    {
+      name: 'Silla Moderna Confortable',
+      image: '/src/assets/images/sillas/241d1421-49d7-467f-8f04-d6711ed4b829.jpg'
+    },
+    {
+      name: 'Silla Minimalista Gris',
+      image: '/src/assets/images/sillas/6e427402-22f5-4820-8f44-8adeff7fac1f.jpg'
+    },
+  ];
+  
+  export default sillas;
--- src/pages/Sillas.jsx
+++ src/pages/Sillas.jsx
@@ -1,9 +1,12 @@
 import SectionBanner from "../components/SectionBanner";
 import productCards from "../data/productData";
+import sillas from "../data/sillaData";
 import ProductCardSales from "../components/ProductCardSales";
 import "./Sillas.css"
 
 export default function Sillas() {
+    
+    
     return (
         <>
             <SectionBanner
@@ -16,10 +19,12 @@ export default function Sillas() {
             <section className="container">
                 <h2>La mejor selección de Sillas</h2>
                 <div className="container-chairs">
-                    <ProductCardSales image={productCards[0].image} title={"Silla1"}/>
-                    <ProductCardSales image={productCards[0].image} title={"Silla2"}/>
-                    <ProductCardSales image={productCards[0].image} title={"Silla3"}/>
-                    <ProductCardSales image={productCards[0].image} title={"Silla4"}/>
+                    {sillas.map(silla => (
+                        <ProductCardSales 
+                            key={silla.name} 
+                            title={silla.name} 
+                            image={silla.image} />
+                    ))}
                 </div>
             </section>
         </>
